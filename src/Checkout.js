99d792847f5d70aeec559bb2603ca5e0bdb66c4f
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2023-06-16 12:33:31 +1000

    lesson 19 - Checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,69 @@
-import Title from "./Title"
-
+import ProductList from "./ProductList";
+import Title from "./Title";
+import { Link } from "react-router-dom";
 export default function Checkout() {
-    return (
-        <div>
-            <Title mainTitle="結賬" />
-            
-        </div>
-    )
+  let cartItem = {
+    cartItems:  [
+        {
+            "id": 5,
+            "name": "藍梅",
+            "image": "blueberry.jpg",
+            "price": 10,
+            "description": "新鮮藍梅50克，補眼之寶",
+            "quantity": 3
+        },
+        {
+            "id": 4,
+            "name": "西瓜",
+            "image": "watermelon.jpg",
+            "price": 20,
+            "description": "新鮮西瓜2公斤，夏季必備",
+            "quantity": 1
+        }
+    ]
+  };
+
+  // destructure
+  let { cartItems } = cartItem;
+  let cartEmpty = cartItems.length <= 0 ? true : false; // check if the cart is empty, if so let Y else N
+
+  return (
+    <>
+      <Title mainTitle="你的購物車" />
+
+      {
+        /* if the cart is empty, then dun display */
+        cartEmpty && (
+          <div>
+            購物車現在沒有商品 <br />
+            <Link to="/"> 去產品頁看看吧</Link>
+          </div>
+        )
+      }
+      {
+        /* if the cart is not empty, if not - which is true, then display */
+        !cartEmpty && (
+          <div>
+            {/* left side - i.e., 產品列表*/}
+            <div id="cartSection">
+              {cartItems.map(product => 
+               (
+                <div key={product.id}>
+                  <img src={process.env.PUBLIC_URL + "/img/" + product.image}/>                 
+                  {product.name}
+                  {product.description}
+                  {product.price}
+                  {product.quantity}
+                </div>
+                )
+              )}
+            </div>
+
+            {/* right side - i  .e., 價錢總數 + 免費送貨*/}
+            <div id="checkOutSection"> </div>
+          </div>
+        )
+      }
+    </>
+  );
 }
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,18 +1,22 @@
 import {Link} from "react-router-dom"
 import styles from './ProductList.module.css'
-import {useState} from "react"  //React Hook
+import {useEffect, useState} from "react"  //React Hook
 import Title from "./Title"
 
 export default function ProductList() {
 
-    let productList = [
-        {"id" : 1,"name" : "蘋果", "price" : 5, "image" : "apple.jpg","description":"新鮮蘋果50克，一日一蘋果，醫生遠離我"},
-        {"id" : 2,"name" : "橙", "price" : 3, "image" : "orange.jpg","description":"新鮮橙50克，又甜又好吃"},
-        {"id" : 3,"name" : "芒果", "price" : 4, "image" : "mango.jpg","description":"新鮮芒500克，宜做甜品"},
-        {"id" : 4,"name" : "西瓜", "price" : 20,"image" : "watermelon.jpg","description":"新鮮西瓜2公斤，夏季必備"},
-        {"id" : 5,"name" : "藍梅", "price" : 10,"image" : "blueberry.jpg","description":"新鮮藍梅50克，補眼之寶"},
-        {"id" : 6,"name" : "白蘿蔔", "price" : 5,"image" : "white-carrot.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"}
-    ]
+    let [productList,setProductList ] = useState ([]) //initial value of productlist， which 係一個空ge array
+
+
+
+    useEffect(() => {
+    // We use fetch() to fetch API information (Json format)
+    fetch('https://hoyinleung.github.io/demoapi/react-basic-product.json')
+    .then(reponse => reponse.json())
+    .then(data => setProductList( data )) 
+    console.log(productList)
+    },
+    [])
 
     return (
         <div>
